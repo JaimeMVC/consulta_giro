e19dac75228517ae2e468a088ebc4c88172c9f13
unknown <jaime.mvc1996@gmail.com>
2025-06-02 21:26:10 -0300

    Corrijo lógica de búsqueda de material

--- stock/views.py
+++ stock/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
-from .models import Material
+from stock.models import Material
 
 def buscar_material(request):
     resultado = None
-    if request.method == 'GET':
-        codigo = request.GET.get('codigo')
-        if codigo:
-            try:
-                resultado = Material.objects.get(codigo=codigo)
-            except Material.DoesNotExist:
-                resultado = "No encontrado"
-    return render(request, 'buscar_material.html', {'resultado': resultado})
+
+    if request.method == "POST":
+        codigo = request.POST.get("codigo", "").strip()
+        try:
+            resultado = Material.objects.get(codigo=codigo)
+        except Material.DoesNotExist:
+            resultado = "No encontrado"
+
+    return render(request, "buscar_material.html", {"resultado": resultado})
